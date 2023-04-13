4413c7b86c7fc521abcffc30337ea3dcea665523
Busenur Adibelli <busenur.adibelli@networksolutionworld.com>
2023-04-13 13:27:10 +0300

    change api url && added ssl

--- src/pages/api/common_songs.js
+++ src/pages/api/common_songs.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export default async function getCommonSongs(playlistUrls) {
   try {
     const response = await axios.post(
-      "http://165.232.123.33:80/common_songs",
+      "https://crusify.badblli.dev/common_songs",
       playlistUrls
     );
     console.log(response.data);
